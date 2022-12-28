f95e852bfd14f1c5a3a0cf88b135ae233f28f290
Angael <krzysztofwidacki@gmail.com>
2022-12-28 15:45:05 +0100

    fix summing limit

--- src/routes/limits/limits-service.ts
+++ src/routes/limits/limits-service.ts
@@ -3,30 +3,43 @@ import { limitsConfig } from '../../utils/limits.js';
 import firebase from '../../firebase/firebase-params.js';
 import { db } from '../../db/db.js';
 
-const getThingSize = async (tableName: string, uid: string) =>
-  (
-    await db
-      .sum({ size: tableName + '.size' })
-      .from(DB_TABLE.item)
-      .join(tableName, 'item.id', tableName + '.item_id')
-      .where({ account_uid: uid })
-  )[0];
+const getThingSize = async (tableName: string, uid: string): Promise<number> => {
+  const response = await db
+    .sum({ size: tableName + '.size' })
+    .from(DB_TABLE.item)
+    .join(tableName, 'item.id', tableName + '.item_id')
+    .where({ account_uid: uid });
+
+  const asNumber = Number(response[0]?.size);
+  if (isNaN(asNumber)) {
+    return 0;
+  } else {
+    return asNumber;
+  }
+};
 
 export async function getLimitsForUser(user: firebase.auth.DecodedIdToken) {
   const { type } = (
     await db.select('account.type').from(DB_TABLE.account).where({ uid: user.uid })
   )[0];
 
-  const { size: imgSize } = await getThingSize(DB_TABLE.image, user.uid);
-  const { size: videoSize } = await getThingSize(DB_TABLE.video, user.uid);
-  const { size: thumbSize } = await getThingSize(DB_TABLE.thumbnail, user.uid);
+  const sizes = await Promise.allSettled([
+    getThingSize(DB_TABLE.image, user.uid),
+    getThingSize(DB_TABLE.video, user.uid),
+    getThingSize(DB_TABLE.thumbnail, user.uid),
+  ]);
 
-  console.log({ imgSize, videoSize, thumbSize });
+  const used = sizes.reduce<number>(
+    (sum, size) => (size.status === 'fulfilled' ? sum + size.value : sum),
+    0,
+  );
+
+  console.log(sizes, used);
 
   return {
     type,
     bytes: {
-      used: +imgSize + +videoSize + +thumbSize,
+      used: used,
       max: limitsConfig[type],
     },
   };
