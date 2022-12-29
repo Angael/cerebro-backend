a0574ac440f88c5b7b24a8e5d823ca232b219c19
Angael <krzysztofwidacki@gmail.com>
2022-12-29 12:51:36 +0100

    user cache; getUserType

--- src/routes/limits/limits-service.ts
+++ src/routes/limits/limits-service.ts
@@ -2,6 +2,8 @@ import { DB_TABLE } from '../../utils/consts.js';
 import { limitsConfig } from '../../utils/limits.js';
 import firebase from '../../firebase/firebase-params.js';
 import { db } from '../../db/db.js';
+import { userTypeCache } from '../../cache/userCache.js';
+import { AccountType } from '../../models/IAccount.js';
 
 const getThingSize = async (tableName: string, uid: string): Promise<number> => {
   const response = await db
@@ -18,10 +20,18 @@ const getThingSize = async (tableName: string, uid: string): Promise<number> => 
   }
 };
 
+export async function getUserType(uid: string): Promise<AccountType> {
+  if (userTypeCache.has(uid)) {
+    return userTypeCache.get(uid);
+  } else {
+    const { type } = (await db.select('account.type').from(DB_TABLE.account).where({ uid }))[0];
+    userTypeCache.set(uid, type);
+    return type;
+  }
+}
+
 export async function getLimitsForUser(user: firebase.auth.DecodedIdToken) {
-  const { type } = (
-    await db.select('account.type').from(DB_TABLE.account).where({ uid: user.uid })
-  )[0];
+  const type = await getUserType(user.uid);
 
   const sizes = await Promise.allSettled([
     getThingSize(DB_TABLE.image, user.uid),
@@ -33,8 +43,6 @@ export async function getLimitsForUser(user: firebase.auth.DecodedIdToken) {
     (sum, size) => (size.status === 'fulfilled' ? sum + size.value : sum),
     0,
   );
-
-  console.log(sizes, used);
 
   return {
     type,
