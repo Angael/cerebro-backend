13bab0259c973bbe8e8bd62ad6b68906916f0813
Angael <krzysztofwidacki@gmail.com>
2023-01-01 10:26:19 +0100

    express routers

--- src/routes/MyRouter.ts
+++ src/routes/MyRouter.ts
@@ -1,15 +1,14 @@
-import express, { Express } from 'express';
+import express from 'express';
 import cors from 'cors';
 import log from '../utils/log.js';
 import { addAuth } from '../middleware/addAuth.js';
 
 import itemRouter from './items/itemRouter.js';
-import registerRoutes from './register/routes.js';
-import limitsRoutes from './limits/routes.js';
+import registerRouter from './register/registerRouter.js';
+import limitsRouter from './limits/limitsRouter.js';
 import { MyRoute } from './express-helpers/routeType.js';
 
-const routes2: ((router: Express) => void)[] = [registerRoutes, limitsRoutes];
-const routes3: MyRoute[] = [itemRouter];
+const routes3: MyRoute[] = [itemRouter, registerRouter, limitsRouter];
 
 const startRouter = () => {
   const router = express();
@@ -20,16 +19,10 @@ const startRouter = () => {
   router.use(addAuth);
   router.get('/', (req, res) => 'v1');
 
-  // v2 way of writing things? more compact
-  routes2.forEach((registerRoutes) => {
-    registerRoutes(router);
-  });
-
-  // v3 with subRouters
   routes3.forEach((myRoute) => {
     router.use(myRoute.path, myRoute.router);
   });
-  router.use(itemRouter.path, itemRouter.router);
+  // router.use(itemRouter.path, itemRouter.router);
 
   router.listen(port, () => {
     log.info(`Router started`);
