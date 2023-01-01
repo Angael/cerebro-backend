b5d82aa6ede1d5e73de9c87552bbf27aee93525b
Angael <krzysztofwidacki@gmail.com>
2023-01-01 11:11:42 +0100

    remove unused code

--- src/cache/userCache.ts
+++ src/cache/userCache.ts
@@ -1,9 +1,5 @@
 import NodeCache from 'node-cache';
 
-export const userLimitsCache = new NodeCache({
-  stdTTL: 60, // 1 min
-});
-
 export const userTypeCache = new NodeCache({
   stdTTL: 600,
 });
