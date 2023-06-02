86564efe7fd543ddb668123712b51dff5e1a81d5
angael <krzysztofwidacki@gmail.com>
2023-06-02 11:24:36 +0200

    use RootProcessor in mediaProcessor

--- src/auto-services/media-processor/mediaProcessor.ts
+++ src/auto-services/media-processor/mediaProcessor.ts
@@ -1,25 +1,51 @@
-import { Scheduler } from 'modern-async';
 import logger from '../../utils/log.js';
 import { processSomeItem } from './processSomeItem.js';
+import { BaseProcessor } from '../base-processor/BaseProcessor.js';
+import { Item, Processed } from '@prisma/client';
+import { prisma } from '../../db/db.js';
 
-const mediaProcessor = new Scheduler(
-  () =>
-    processSomeItem()
-      .then((id) => {
-        if (!id === null) {
-          // some item was optimized!
-          logger.verbose('Optimized itemId %i', id);
-        }
-      })
-      .catch((error) => {
-        logger.error('Item not optimized: %O', error);
-      }),
-  4000,
-  {
-    concurrency: 1,
-    maxPending: 1,
-    startImmediate: true,
+const mediaProcessor = new BaseProcessor<Item>({
+  concurrency: 1,
+  processItem: processSomeItem,
+  checkInterval: 5000,
+
+  getItems() {
+    return prisma.item.findMany({
+      take: 10,
+      where: { processed: Processed.NO },
+    });
   },
-);
+
+  async canProcessItem(item) {
+    const itemRow = await prisma.item.findFirst({
+      where: { id: item.id },
+    });
+
+    return itemRow?.processed === Processed.NO;
+  },
+
+  async setItemStarted(item) {
+    await prisma.item.update({
+      where: { id: item.id },
+      data: { processed: Processed.STARTED },
+    });
+  },
+
+  async setItemProcessed(item) {
+    logger.verbose('Processed item %i', item.id);
+    await prisma.item.update({
+      where: { id: item.id },
+      data: { processed: Processed.V1 },
+    });
+  },
+
+  async onItemError(item, error) {
+    logger.error('Error processing item %i', item.id);
+    await prisma.item.update({
+      where: { id: item.id },
+      data: { processed: Processed.FAIL },
+    });
+  },
+});
 
 export default mediaProcessor;
