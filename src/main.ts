260b12d76894120647f5653120423aba910579fc
Angael <krzysztofwidacki@gmail.com>
2023-06-02 22:56:44 +0200

    Remove RootScheduler.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import mediaProcessor from './auto-services/media-processor/mediaProcessor.js';
 
 import './prepare.js';
 import videoCompressor from './auto-services/video-compressor/videoCompressor.js';
-import { RootScheduler } from './auto-services/RootScheduler.js';
 
-RootScheduler.start();
 mediaProcessor.start();
 videoCompressor.start();
 startRouter();
