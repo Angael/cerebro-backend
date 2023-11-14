4a2b6664ef62268668bdd10704d616a8781b47eb
Angael <krzysztofwidacki@gmail.com>
2023-11-14 23:54:00 +0100

    forRoles implementation

--- src/middleware/forRoles.ts
+++ src/middleware/forRoles.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
+import { UserType } from '@prisma/client';
 
-export const forRoles = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: implement
-  throw new Error('Not implemented');
-  next();
-};
+export const forRole =
+  (role: keyof typeof UserType) => (req: Request, res: Response, next: NextFunction) => {
+    if (req.auth?.userId && req.auth.sessionClaims.roles.includes(role)) {
+      next();
+    } else {
+      res.status(403).send();
+    }
+  };
